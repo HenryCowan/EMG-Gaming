37a2d6f190e7d72013b82bff045964b3ad5d242b
HenryCowan <61460487+HenryCowan@users.noreply.github.com>
2020-02-26 16:54:52 +0000

    Update window.h

--- window.h
+++ window.h
@@ -1,3 +1,4 @@
+
 #ifndef WINDOW_H
 #define WINDOW_H
 
@@ -8,7 +9,7 @@
 
 #include <QBoxLayout>
 
-// #include "adcreader.h"
+#include "adcreader.h"
 
 // class definition 'Window'
 class Window : public QWidget
@@ -23,25 +24,21 @@ public:
 
 	void timerEvent( QTimerEvent * );
 
-
+public slots:
+	void setGain(double qgain);
 
 // internal variables for the window class
 private:
-
-	QwtPlot      *plot1;
-	QwtPlot      *plot2;
-	QwtPlot      *plot3;
-	QwtPlot      *plot4;
-
-
-	QwtPlotCurve *curve1;
-	QwtPlotCurve *curve2;
-	QwtPlotCurve *curve3;
-	QwtPlotCurve *curve4;
+	QwtKnob      *knob;
+	QwtThermo    *thermo;
+	QwtPlot      *plot;
+	QwtPlotCurve *curve;
+	
+	Adafruit_ADS1015 ads;
+	uint16_t adc0;
 
 	// layout elements from Qt itself http://qt-project.org/doc/qt-4.8/classes.html
-	QVBoxLayout  *vLayout1;  // vertical layout
-        QVBoxLayout  *vLayout2;  // vertical layout
+	QVBoxLayout  *vLayout;  // vertical layout
 	QHBoxLayout  *hLayout;  // horizontal layout
 
 	static const int plotDataSize = 100;
@@ -50,7 +47,7 @@ private:
 	double xData[plotDataSize];
 	double yData[plotDataSize];
 
-
+	double qgain;
 	int count;
 
 //	ADCreader *adcreader;
