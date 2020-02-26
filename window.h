2da81462f0b166a3c8ef68e9dc1b268706073f6f
HenryCowan <61460487+HenryCowan@users.noreply.github.com>
2020-02-26 09:45:57 +0000

    Update window.h

--- window.h
+++ window.h
@@ -23,18 +23,25 @@ public:
 
 	void timerEvent( QTimerEvent * );
 
-public slots:
-	void setGain(double gain);
+
 
 // internal variables for the window class
 private:
-	QwtKnob      *knob;
-	QwtThermo    *thermo;
-	QwtPlot      *plot;
-	QwtPlotCurve *curve;
+
+	QwtPlot      *plot1;
+	QwtPlot      *plot2;
+	QwtPlot      *plot3;
+	QwtPlot      *plot4;
+
+
+	QwtPlotCurve *curve1;
+	QwtPlotCurve *curve2;
+	QwtPlotCurve *curve3;
+	QwtPlotCurve *curve4;
 
 	// layout elements from Qt itself http://qt-project.org/doc/qt-4.8/classes.html
-	QVBoxLayout  *vLayout;  // vertical layout
+	QVBoxLayout  *vLayout1;  // vertical layout
+        QVBoxLayout  *vLayout2;  // vertical layout
 	QHBoxLayout  *hLayout;  // horizontal layout
 
 	static const int plotDataSize = 100;
@@ -43,7 +50,7 @@ private:
 	double xData[plotDataSize];
 	double yData[plotDataSize];
 
-	double gain;
+
 	int count;
 
 //	ADCreader *adcreader;
