fa607ddc6a1acfe10cf73a13e9c9324a1c0fdc7c
Zonghan-Barry-Gan <barry126g@gmail.com>
2020-04-07 10:39:49 +0100

    udp to send arry

--- Server_end/0225 UDPtest unit/UDPrsvertest/UdpReceiver.cpp
+++ Server_end/0225 UDPtest unit/UDPrsvertest/UdpReceiver.cpp
@@ -12,8 +12,8 @@ Udprsvertest::Udprsvertest(QObject *p) :
     //bind local address and port for receiving
     //bool bdrsvsc = rsverSocket->bind(QHostAddress("172.30.141.244"), PORT);
     //bool bdrsvsc = rsverSocket->bind(QHostAddress("172.30.156.14"), PORT);
-    bool bdrsvsc = rsverSocket->bind(QHostAddress("127.0.0.1"), PORT);
-    //bool bdrsvsc = rsverSocket->bind(QHostAddress::LocalHost, PORT);
+
+    bool bdrsvsc = rsverSocket->bind(QHostAddress::LocalHost, PORT);
     if(bdrsvsc>0)
       { qDebug()<<"bind success";
         qDebug() << "--- receiving--";
@@ -33,18 +33,28 @@ Udprsvertest::~Udprsvertest()
 
 void Udprsvertest::receive()
 {
-    QByteArray dtstrm;
+//    QByteArray dtstrm;
     while(rsverSocket->hasPendingDatagrams())
     {
-        dtstrm.resize(rsverSocket->pendingDatagramSize());
-        rsverSocket->readDatagram(dtstrm.data(), dtstrm.size());
+//        dtstrm.resize(rsverSocket->pendingDatagramSize());
 
-        bool cktp2;
-        const float outval= dtstrm.toFloat(&cktp2);
-        if (!cktp2) qDebug() << "received but data error, data type Conversion failed";
 
-        qDebug() << "data: " << outval;
+
+
+        //  QByteArray transform back into float[]
+        float  outFval[4];
+
+        //float  fVar[4] = { 1.0, 1.0, 1.0, 1.0 };//set size of any array with 4 float
+
+        //int len_fVar = sizeof(fVar); // 4*4 = 16 calculate the size
+        rsverSocket->readDatagram((char*)outFval, sizeof(outFval));
+
+//        memcpy(&outFval, dtstrm.data(), len_fVar);
+
+
+        qDebug() << "data: " << outFval[0]<<outFval[1]<<outFval[2]<<outFval[3];
     }
 }
+
 //inspired by https://blog.csdn.net/weixin_42216430/article/details/80780297
 
--- Server_end/0225 UDPtest unit/UDPsdertest/main.cpp
+++ Server_end/0225 UDPtest unit/UDPsdertest/main.cpp
@@ -16,46 +16,31 @@ int main(int argc, char *argv[])
     QUdpSocket sdersc;
     //bind local address and check
     //if(sdersc.bind(QHostAddress("192.168.43.231"), sderprt)>0);
-  //  if(sdersc.bind(QHostAddress::LocalHost, sderprt)>0);
-    if(sdersc.bind(QHostAddress("127.0.0.1"), sderprt)>0)
-     {       
-	qDebug()<<"sender sc bind sucess";
-     }
-//    sdersc.bind(QHostAddress("172.30.156.14"), sderprt);
-//    QByteArray msg = "Hello world!";
-
-//    int  dt[4] = {0x3,0x3,0x3,0x3};//初始化变量赋值
-
-//    QByteArray msg;
-//    int len_dt = sizeof(dt); // 4*4 = 16 (float占4个字节)
-//    msg.resize(len_dt);
-//    memcpy(msg.data(), &dt, len_dt);
-//    int  outIntVar[4];
-//    memcpy(&outIntVar, msg.data(), len_dt);
-
+    if(sdersc.bind(QHostAddress::LocalHost, sderprt)>0);
+        qDebug()<<"sender sc bind sucess";
     float dt = 20.0;
     QByteArray msg;
-    for(int i=0; i<10000000; i++){
+    for(int i=0; i<10000; i++){
+        // float[] transform to QByteArray
+        float  fVar[4] = { 1.1, 2.3, 9.5, 0.2 };//initialize to-be-sent dataset
 
-        dt=dt*-1.0;
-        msg.setNum(dt);
-        bool cktp;
-        const float outval= msg.toFloat(&cktp);
-        if (!cktp and outval != dt) qDebug() << "before sending data, data type Conversion failed";
+// transforming float into qbytearray
+        int len_fVar = sizeof(fVar); // 4*4 = 16 bit
+//        msg.resize(len_fVar);
+//        memcpy(msg.data(), &fVar, len_fVar);
+
         qDebug() << "--- Sending";
-        qDebug() << "data: " << msg;
- //       qDebug() << "sendingtest " << msg;
-        qDebug() << "sendingtest " << outval;
+//        qDebug() << "data: " << msg;
+
         //send and check
 //        bool cksd = sdersc.writeDatagram(msg, QHostAddress("192.168.43.30"), rscverprt);
-        bool cksd = sdersc.writeDatagram(msg, QHostAddress("127.0.0.1"), rscverprt);
-        //bool cksd = sdersc.writeDatagram(msg, QHostAddress::AnyIPv4, 1112);
-        //bool cksd = sdersc.writeDatagram(msg, QHostAddress::LocalHost, 1112);
+        bool cksd = sdersc.writeDatagram((char*)fVar,len_fVar,QHostAddress("127.0.0.1"), rscverprt);
+        qDebug()<<fVar[0]<<fVar[2];
         if(cksd>0)
             qDebug()<<"successfully send";
         else
         {
-        //可调用error()函数，查看bind不成功的原因
+        //see the fail reason with error()
             qDebug()<<"sending is failed";
             qDebug()<<sdersc.error();
 
@@ -65,5 +50,6 @@ int main(int argc, char *argv[])
         }
     return a.exec();
 }
+
 
 // inspired by https://blog.csdn.net/weixin_42216430/article/details/80780297
