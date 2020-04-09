c222b5a38c09db92c744ba9fbe38b5872dfdb80c
HenryCowan <61460487+HenryCowan@users.noreply.github.com>
2020-04-09 15:15:42 +0100

    Update main.cpp

--- Server_end/0301 Pong GUI/main.cpp
+++ Server_end/0301 Pong GUI/main.cpp
@@ -1,10 +1,15 @@
 #include <QApplication>
 #include "mainwindow.h"
 #include <QScreen>
+/**
+ * The <code>main</code> function is used to find and update
+ * the mainwindow with the optimum screen and display size
+ * for android applications using Qt
+ */
 
 int main(int argc, char *argv[])
 {
-    //automatically adjust dpi of display
+    //*automatically adjust dpi of display*/
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QApplication a(argc, argv);
 	/*Detecting the screen size and adjust the display size in according
@@ -12,7 +17,7 @@ int main(int argc, char *argv[])
 	*/
     QScreen *iScreen = a.primaryScreen();
     QSize rScreenSize = iScreen->size();
-    //pass screen size to mainwindow as argument
+    //*pass screen size to mainwindow as argument*/
 	MainWindow w(rScreenSize.rwidth(),rScreenSize.rheight());
     w.show();
 
